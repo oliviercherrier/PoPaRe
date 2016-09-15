9daebc7d663ca7806fd8a15eed2184adcb545b40
Olivier Cherrier <olivier.cherrier@gmail.com>
2016-09-15 16:19:57 +0200

    Add basges section and mumy progress bar

--- app/pages/statistics/statistics.ts
+++ app/pages/statistics/statistics.ts
@@ -8,6 +8,7 @@ import { CHART_DIRECTIVES } from 'angular2-highcharts';
   Ionic pages and navigation.
 */
 
+
 @Component({
   selector: 'statistics-page',
   directives: [CHART_DIRECTIVES],
@@ -82,6 +83,78 @@ export class StatisticsPage {
         {name: 'Poids', data: [8,9,10,10]}
       ]
     };
+
+    this.mumyProgressBarOptions = {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+          text: undefined
+        },
+        credits: {
+          enabled: false
+        },
+        xAxis: {
+          lineWidth: 0,
+          minorGridLineWidth: 0,
+          lineColor: 'transparent',      
+          labels: {
+            enabled: false
+          },
+          minorTickLength: 0,
+          tickLength: 0,
+          gridLineWidth: 0,
+        },   
+        yAxis: {
+          type: 'datetime',
+          tickInterval: 3600 * 1000, //1 hour
+          min: 0,
+          dateTimeLabelFormats: {
+              millisecond: '%H:%M:%S',
+              second: '%H:%M:%S',
+              minute: '%H:%M:%S',
+              hour: '%H:%M:%S',
+              day: '%H:%M:%S',
+              week: '%H:%M:%S',
+              month: '%H:%M:%S',
+              year: '%H:%M:%S'
+          },
+          labels: {
+            enabled: false
+          },
+          gridLineWidth: 0,
+          title: {
+            text: ''
+          }
+        },
+        legend: {
+          reversed: true,
+          enabled: false
+        },
+        plotOptions: {
+          series: {
+            stacking: 'normal',
+          },
+          bar: {
+            dataLabels: {
+               formatter: function(){
+                let date = new Date(this.y);
+                return "<span style=\"font-size: 80%\">"+date.getUTCHours().toString() + "h" + date.getUTCMinutes().toString() + "</span>";
+              },
+              enabled: true,
+              color: 'white',
+              style: {
+                textShadow: '0 0 3px black'
+              }
+            }
+          },
+        },
+
+        series: [
+          { data: [Date.UTC(1970, 0, 1, 3,46)]},
+          { data: [Date.UTC(1970, 0, 1, 4,13)]
+        }]
+    };
   }
 
  ionViewDidEnter(){
@@ -89,7 +162,8 @@ export class StatisticsPage {
     setTimeout(
       function(){
         self.mumyWeightChart.reflow(); 
-        self.babyWeightchart.reflow()
+        self.babyWeightchart.reflow();
+        self.mumyProgressBar.reflow();
       },
       1000
     );
@@ -98,12 +172,16 @@ export class StatisticsPage {
 
   }
 
-  saveMumyWeightInstance(chartInstance) {
+  saveMumyWeightChart(chartInstance) {
       this.mumyWeightChart = chartInstance;
   }
 
-  saveBabyWeightInstance(chartInstance) {
+  saveBabyWeightChart(chartInstance) {
       this.babyWeightchart = chartInstance;
+  }
+
+  saveMumyProgressBar(chartInstance) {
+      this.mumyProgressBar = chartInstance;
   }
 
   mumyWeightChart : HighchartsChartObject;
@@ -112,4 +190,6 @@ export class StatisticsPage {
   babyWeightchart : HighchartsChartObject;
   babyWeightChartOptions: HighchartsOptions;
 
+  mumyProgressBar: HighchartsChartObject;
+  mumyProgressBarOptions: HighchartsOptions;
 }
